3eb368b56a9ed6618727943701ecd783e5964599
FELYPE RAMIREZ ALVES <113054755+FelypeRA@users.noreply.github.com>
2023-01-10 08:38:30 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+print('-'*40)
+print('ferramenta para cadastro de funcionarios ')
+print('-'*40)
+
+funcinario = str(input('Qual o nome do funcionário? '))
+idade = int(input('Qual a idade? '))
+setor = str(input('Qual a profissão?  '))
