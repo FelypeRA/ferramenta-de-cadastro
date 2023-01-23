abca608eacf33260f9b2100f3cff1e844e57ee99
FELYPE RAMIREZ ALVES <113054755+FelypeRA@users.noreply.github.com>
2023-01-23 11:18:07 -0300

    create menu.py

---
+++ menu.py
@@ -0,0 +1,10 @@
+def menu():
+    print('-'*60)
+    print(f'{"ferramenta para cadastro de funcionarios":^60}')
+    print('-'*60)
+    print('         [ 1 ] Para cadastrar um funcionário. ')
+    print('         [ 2 ] Para ver lista de funcionários.')
+    print('         [ 3 ] Para excluir um funcionario.   ')
+    print('         [ 4 ] Sair do programa.              ')
+    print('-' * 60)
+ 
